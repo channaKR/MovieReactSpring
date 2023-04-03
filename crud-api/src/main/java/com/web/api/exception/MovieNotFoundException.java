e6f79c44727f6a276444f693e9b69f83e88e75ae
channaKR <channar.official@gmail.com>
2023-04-03 20:20:50 +0530

    add update

---
+++ crud-api/src/main/java/com/web/api/exception/MovieNotFoundException.java
@@ -0,0 +1,31 @@
+package com.web.api.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class MovieNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	@ResponseBody
+    @ExceptionHandler(MovieNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+	public Map<String,String> MovieNotFoundException(){
+
+        Map<String,String> errorMap=new HashMap<>();
+        errorMap.put("errorMessage","user not found");
+
+        return errorMap;
+
+    }
+}
--- crud-api/src/main/java/com/web/api/service/MovieImplement.java
+++ crud-api/src/main/java/com/web/api/service/MovieImplement.java
@@ -1,10 +1,13 @@
 package com.web.api.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.web.api.exception.MovieNotFoundException;
+import com.web.api.exception.UserNotFoundException;
 import com.web.api.model.Movie;
 import com.web.api.repo.MovieRepositary;
 
@@ -22,6 +25,37 @@ public class MovieImplement implements MovieService {
 		// TODO Auto-generated method stub
 		return movierepositary.findAll();
 	}
+	@Override
+	public Optional<Movie> searchById(Integer id) {
+		// TODO Auto-generated method stub
+		
+		return movierepositary.findById(id);
+		
+	}
+	@Override
+	public Movie getMovie(Integer id) {
+		// TODO Auto-generated method stub
+		return movierepositary.findById(id).orElseThrow(()->new MovieNotFoundException());
+	}
+	
+	public Movie updateMovie(Integer id,Movie movies) {
+		return movierepositary.findById(id).map(
+				
+				movie->{
+					
+					movie.setMoviename(movies.getMoviename());
+					movie.setMovieDescription(movies.getMovieDescription());
+					return movierepositary.save(movie);
+					
+				}
+				
+				).orElseThrow(()->new UserNotFoundException(id));
+		
+		
+		
+	}
+	
+	
 
 
 
